9caa5f063526a8eb886591dd11de46b5f2fac73c
E2394 <sametcoban2009.gmail.com>
2020-07-17 01:46:42 +0200

    FizzBuzz assignment...

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+numbers = list(range(1,101))
+for i in numbers:
+    if i%3 == 0 and i%5 == 0:
+        numbers[i-1]="FizzBuzz" 
+    elif i%3 == 0 and i%5 !=0:
+        numbers[i-1]="Fizz"
+    elif i%3 !=0 and i%5 ==0:
+        numbers[i-1]="Buzz"
+    else:
+        numbers[i-1]=i
+    i+=1
+print(numbers)
